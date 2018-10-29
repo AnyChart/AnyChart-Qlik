550d155bf7e29559be66451e67caeff0180b250c
chernetsky <chernetsky@gmail.com>
2018-10-29 15:16:22 +0800

    ENV-1111. Fonts override problem fix

--- anychart-qlik.js
+++ anychart-qlik.js
@@ -28,6 +28,17 @@ define([
       var hCubeHeight = Math.floor(10000 / Math.min(10000, hCubeWidth));
       var documentURI = null;
 
+      //remove default Qlik font-family for SVG objects
+      for (var i = 0; i < document.styleSheets.length; i++) {
+        var sheet = document.styleSheets[i];
+        for (var j = 0; j < sheet.cssRules.length; j++) {
+          var rule = sheet.cssRules[j];
+          if (rule.style && rule.selectorText == ".qv-object *") {
+            rule.style.removeProperty("font-family");
+          }
+        }
+      }
+
       return {
         initialProperties: {
           version: 1.0,
@@ -125,9 +136,9 @@ define([
                     'Also you can use multiple measures and dimensions. Please use "Dimensions", "Measures" and "Sorting" tabs to setup your data:' +
                     '<div class="screenshot screenshot-2"></div></div></div>';
 
-                  $element.html(str);
-                }
+                $element.html(str);
               }
+            }
 
 
           return qlik.Promise.resolve();
