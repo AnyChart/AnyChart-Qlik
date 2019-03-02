cb0fc3cf18b8bf03a386630b7a0abef82a498916
chernetsky <chernetsky@gmail.com>
2019-03-02 17:02:25 +0800

    css are local

--- 001_mekko/001_mekko.js
+++ 001_mekko/001_mekko.js
@@ -1,12 +1,33 @@
 requirejs.config({
   config: {
     'plugin': {
-      type: 'mekko'
+      // localization: {
+      //   // inputLocale: "",
+      //   // outputLocale: "",
+      //   // inputDateTimeFormat: "",
+      //   // outputDateFormat: "",
+      //   // outputDateTimeFormat: "",
+      //   // outputTimeFormat: "",
+      //   // outputTimezone: ""
+      // },
+
+      type: 'mekko',
+      minDimentions: 1,
+      minMeasures: 1
     }
   },
-  paths: {'plugin': 'http://127.0.0.1:8080/anychart-qlik'}
+  paths: {
+    'plugin': 'http://127.0.0.1:8080/anychart-qlik'
+  }
 });
 
-define(['plugin'],function(plugin){
+define([
+    'css!./css/style.css',
+    'css!./css/anychart-editor.min.css',
+    'css!./css/anychart-font.min.css',
+    'css!./css/anychart-ui.min.css',
+
+    'plugin'
+],function(css1, css2, css3, css4, plugin){
   return plugin;
 });
